9e1d33a6a9d3872c6619b64c0f595b6721d3c7b7
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-11-29 13:20:00 -0500

    Update transient.php

--- 01-14-start/transient.php
+++ 01-14-start/transient.php
@@ -10,15 +10,28 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
- function lil_save_display_name(  $user_login, $user ) {
 
+// store data so we don't have to keep retrieving 
+//  it to save on performance. 
+
+
+ function lil_save_display_name(  $user_login, $user ) {
+	set_transient( 'lil_display_name', $user->display_name, (HOUR_IN_SECONDS*24*30 ) );
  }
 
  add_action( 'wp_login', 'lil_save_display_name', 10, 2 );
 
- function lil_show_transient() {
+ function lil_delete_display_name() {
+	delete_transient( 'lil_display_name' );
+ }
 
-	return '';
+ add_action( 'wp_logout', 'lil_delete_display_name' );
+
+ function lil_show_transient() {
+	$display_name = ( ! empty( get_transient( 'lil_display_name' ) ) ) ? get_transient( 'lil_display_name' ) :
+	'There';
+
+	return '<h2>Hello, '. $display_name . '</h2>';
 }
 
  function lil_shortcodes_init(){
