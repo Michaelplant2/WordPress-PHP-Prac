fc36b9d40b848d25a4c0e25ea3cd7bcc1f2fc075
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-11-29 13:02:15 -0500

    Update greet-user.php

--- 01-13-start/greet-user.php
+++ 01-13-start/greet-user.php
@@ -10,16 +10,24 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
+
+// When user logs in they get greeted by first name or user name
+
  function lil_login_redirect() {
-	
+	return trailingslashit( home_url() ) . 'logged_in';
  }
  
  // Replace the **Hook** with the Appropriate One
- add_action( 'admin_init', 'lil_login_redirect');
+ add_filter( 'login_redirect', 'lil_login_redirect');
 
  function lil_show_username() {
+	$name = 'There';
 
-	return '';
+	if ( is_user_logged_in() ) {
+		$user = get_userdata( get_current_user_id() );
+		$name = $user->display_name;
+	}
+	return '<h2>Hello, ' . $name . '</h2>';
 }
 
  function lil_shortcodes_init(){
